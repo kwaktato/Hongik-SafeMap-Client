53ab84bd9407d412e78ce09db4318432bdefcd60
kwaktato <109459319+kwaktato@users.noreply.github.com>
2026-05-04 12:12:13 +0900

    feat: 사용자 행동요령 페이지

---
+++ src/pages/user/Guide/GuideDetailPage.tsx
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Back from '@/assets/icons/ChevronLeft.svg?react';
+import { useSafetyTipsByDisasterType } from '@/api/safetyTips';
+import { NavBar } from '@/components/common/NavBar';
+import { WarningSection } from '@/components/user/guide/WarningSection';
+import { SuppliesSection } from '@/components/user/guide/SuppliesSection';
+import { ActionSection } from '@/components/user/guide/ActionSection';
+import { useHandleNavigate } from '@/hooks/useHandleNavigate';
+
+export const GuideDetailPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const { handleGoBack } = useHandleNavigate();
+
+  const { data } = useSafetyTipsByDisasterType(Number(id));
+
+  const [activeTab, setActiveTab] = useState('행동 요령');
+  const handleTabChange = (tabName: string) => {
+    setActiveTab(tabName);
+    window.scrollTo(0, 0);
+  };
+
+  const tabContent = {
+    '행동 요령': <ActionSection guides={data?.actions} />,
+    '필수 준비물': <SuppliesSection supplies={data?.supplies} />,
+    주의사항: <WarningSection warnings={data?.warnings} />,
+  };
+
+  return (
+    <Container>
+      <NavBar
+        left={<Back onClick={handleGoBack} />}
+        center={<NavCenter>{data?.title}</NavCenter>}
+      />
+
+      <Tabs>
+        {['행동 요령', '필수 준비물', '주의사항'].map((tab) => (
+          <Tab
+            key={tab}
+            active={activeTab === tab}
+            onClick={() => handleTabChange(tab)}
+          >
+            {tab}
+          </Tab>
+        ))}
+      </Tabs>
+
+      {tabContent[activeTab as keyof typeof tabContent]}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin: 0px 20px;
+  position: relative;
+`;
+
+const NavCenter = styled.div`
+  color: ${({ theme }) => theme.colors.gray1000};
+  font-size: ${({ theme }) => theme.font.fontSize.title20};
+  font-weight: ${({ theme }) => theme.font.fontWeight.bold};
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  justify-content: space-around;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray300};
+  background: ${({ theme }) => theme.colors.white};
+
+  margin: 0 -20px;
+  padding: 0 20px;
+  position: sticky;
+  top: 56px;
+  z-index: 1;
+`;
+
+const Tab = styled.div<{ active: boolean }>`
+  padding: 0px 8px;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  cursor: pointer;
+  color: ${({ theme, active }) =>
+    active ? theme.colors.red600 : theme.colors.gray1000};
+  font-size: ${({ theme }) => theme.font.fontSize.detail12};
+  font-weight: ${({ theme }) => theme.font.fontWeight.medium};
+  padding-bottom: 2px;
+  border-bottom: ${({ active }) => (active ? '2px solid #D4182E' : '')};
+`;
---
+++ src/pages/user/Guide/GuidePage.tsx
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+import { useMemo, useState } from 'react';
+import Bookmark from '@/assets/icons/BookmarkDisactiveS.svg?react';
+import { useSafetyTipSummary } from '@/api/safetyTips';
+import { TitleHeader } from '@/components/common/TitleHeader';
+import { SearchBar } from '@/components/common/SearchBar';
+import { GuideCard } from '@/components/user/guide/GuideCard';
+import { Tag } from '@/components/common/Tag';
+
+export const GuidePage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [favorites, setFavorites] = useState<string[]>([]);
+  const [showFavoritesOnly, setShowFavoritesOnly] = useState(false);
+
+  const { data } = useSafetyTipSummary();
+
+  const toggleFavorite = (id: string, e: React.MouseEvent) => {
+    e.stopPropagation();
+    setFavorites((prev) =>
+      prev.includes(id) ? prev.filter((fav) => fav !== id) : [...prev, id],
+    );
+  };
+
+  const filteredGuides = useMemo(() => {
+    let guides = data;
+
+    if (showFavoritesOnly) {
+      guides = guides?.filter((guide) => favorites.includes(guide.id));
+    }
+
+    if (searchTerm) {
+      guides = guides?.filter(
+        (guide) =>
+          guide.title.includes(searchTerm) || guide.detail.includes(searchTerm),
+      );
+    }
+
+    return guides;
+  }, [searchTerm, showFavoritesOnly, data]);
+
+  return (
+    <Container>
+      <TitleHeader
+        mainTitle="유형별 행동 요령"
+        subTitle="재난 상황에서 안전을 지키는 필수 행동 지침을 확인하세요"
+      />
+
+      <SearchBar
+        placeholder="행동 요령 검색"
+        value={searchTerm}
+        onSearch={() => setSearchTerm(searchTerm)}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onClear={() => setSearchTerm('')}
+        iconPosition="left"
+      />
+
+      <Tag
+        variant={showFavoritesOnly ? 'red' : 'white'}
+        onClick={() => setShowFavoritesOnly(!showFavoritesOnly)}
+        style={{
+          width: '84px',
+          height: '20px',
+          padding: '6px 4px 6px 8px',
+        }}
+      >
+        북마크만 보기
+        <Bookmark />
+      </Tag>
+
+      <CardWrapper>
+        {filteredGuides?.map((guide) => {
+          const isFavorite = favorites.includes(guide.id);
+
+          return (
+            <GuideCard
+              key={guide.id}
+              guide={guide}
+              isFavorite={isFavorite}
+              onToggleFavorite={toggleFavorite}
+            />
+          );
+        })}
+      </CardWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin: 72px 20px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
+`;
+
+const CardWrapper = styled.div`
+  display: grid;
+  gap: 8px;
+
+  grid-template-columns: repeat(1, 1fr);
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
