932f6e606c6c28b857124f67166bafc80b82f61c
kwaktato <109459319+kwaktato@users.noreply.github.com>
2026-05-04 12:07:35 +0900

    feat: 기본 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import { Route, Routes } from 'react-router-dom';
-import SplashPage from '@/pages/login/SplashPage';
-import LoginPage from '@/pages/login/LoginPage';
-import SignupPage from '@/pages/signup/SignupPage';
-import ErrorPage from '@/pages/ErrorPage';
-import UserRoute from '@/route/UserRoute';
-import AdminRoute from '@/route/AdminRoute';
+import { useMaintenance } from '@/contexts/MaintenanceContext';
+import { SplashPage } from '@/pages/login/SplashPage';
+import { LoginPage } from '@/pages/login/LoginPage';
+import { SignupPage } from '@/pages/signup/SignupPage';
+import { UserRoute } from '@/route/UserRoute';
+import { AdminRoute } from '@/route/AdminRoute';
+import { ErrorPage } from '@/pages/ErrorPage';
+import { MaintenancePage } from '@/pages/MaintenancePage';
+import { useEffect } from 'react';
 
 function App() {
+  const { isMaintenance, setMaintenance } = useMaintenance();
+
+  useEffect(() => {
+    const handleMaintenance = () => setMaintenance(true);
+
+    // 이벤트 리스너 등록
+    window.addEventListener('maintenance-mode', handleMaintenance);
+    return () =>
+      window.removeEventListener('maintenance-mode', handleMaintenance);
+  }, [setMaintenance]);
+
+  if (isMaintenance) {
+    return <MaintenancePage />;
+  }
+
   return (
     <Routes>
       <Route path="/" element={<SplashPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+
+      <Route path="/maintenance" element={<MaintenancePage />} />
 
       <Route path="/user/*" element={<UserRoute />} />
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,37 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from '@/style/GlobalStyle.tsx';
 import { theme } from '@/style/theme.tsx';
 import App from '@/App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { MaintenanceProvider } from '@/contexts/MaintenanceContext';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error: any) => {
+      if (
+        error.response?.status === 503 &&
+        !window.location.pathname.startsWith('/admin')
+      ) {
+        window.dispatchEvent(new Event('maintenance-mode'));
+      }
+    },
+  }),
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <MaintenanceProvider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </MaintenanceProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
