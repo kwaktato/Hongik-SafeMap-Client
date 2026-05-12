1ff01ba8f7617c45428f04f7469b1639510be4c8
kwaktato <109459319+kwaktato@users.noreply.github.com>
2026-05-13 02:58:41 +0900

    fix: 관리자 재난 제보 상세보기 수정

--- src/components/admin/settings/Record/ModalReport.tsx
+++ src/components/admin/settings/Record/ModalReport.tsx
@@ -1,104 +1,117 @@
 import styled from 'styled-components';
+import ModalClose from '@/assets/icons/Exit.svg?react';
 import Count from '@/assets/icons/InformationXS.svg?react';
 import Clock from '@/assets/icons/ClockXS.svg?react';
 import Position from '@/assets/icons/PositionXS.svg?react';
-import Type from '@/assets/icons/NotificationXS.svg?react';
 import CheckIcon from '@/assets/icons/ExactXS.svg?react';
 import FalseIcon from '@/assets/icons/FalseXS.svg?react';
 import { Tag, type TagColor } from '@/components/common/Tag';
-import { Button } from '@/components/common/Button';
-import type { DisasterGroupDetail } from '@/types/Report';
 import { formatDateTime, formatSimpleDate } from '@/utils/formatDate';
 import { formatAddress, type KakaoAddressResult } from '@/utils/formatAddress';
+import { useAdminReportGroupDetail } from '@/api/admin';
 
 interface ModalReportProps {
   onClose: () => void;
   addressData: KakaoAddressResult | null;
-  group: DisasterGroupDetail | undefined;
+  disasterTitle: string;
+  groupId: number;
 }
 
 export const ModalReport = ({
   onClose,
   addressData,
-  group,
+  disasterTitle,
+  groupId,
 }: ModalReportProps) => {
+  const { data: group } = useAdminReportGroupDetail(groupId);
+
   if (!group) return null;
 
   const tag = {
     긴급: {
       variant: 'red' as TagColor,
       text: '긴급',
+      level: '매우 심각',
     },
     높음: {
       variant: 'red' as TagColor,
       text: '높음',
+      level: '심각',
     },
     보통: {
       variant: 'black' as TagColor,
       text: '보통',
+      level: '보통',
     },
     낮음: {
       variant: 'white' as TagColor,
       text: '낮음',
+      level: '낮음',
     },
   };
-
   const riskTag = tag[group.latestRiskLevel];
 
   return (
     <ModalWrapper>
-      <TitleWrapper>
-        <div className="top">
-          <div className="title">{group.disasterType.name}</div>
-          <div className="right">
-            <div>현재 위험도</div>
+      <DisasterWrapper>
+        <div className="row">
+          <div className="modal">재난 제보 기록</div>
+          <ModalXImg onClick={onClose} />
+        </div>
+
+        <div className="title">{disasterTitle}</div>
+
+        <div className="row10">
+          <div className="row4">
+            <div className="tag">재난 유형</div>
+            <Tag variant="black">{group.disasterType.name}</Tag>
+          </div>
+          <div className="divider" />
+          <div className="row4">
+            <div className="tag">현재 위험도</div>
             <Tag variant={riskTag.variant}>{riskTag.text}</Tag>
           </div>
         </div>
 
-        <div className="info">
-          <div className="detail">
-            <Count />
-            제보 건수
-          </div>
-          {group.reportCount}건
-        </div>
-
-        <div className="info">
-          <div className="detail">
-            <Clock />
-            발생 기간
-          </div>
-          {formatSimpleDate(group.earliestReportTime, true)} ~{' '}
-          {formatSimpleDate(group.latestReportTime, true)}
-        </div>
-
-        {addressData && (
+        <InfoWrapper>
           <div className="info">
-            <div className="detail">
-              <Position />
-              발생 장소
+            <div className="left">
+              <Count />
+              제보 건수
             </div>
-            {formatAddress(addressData, true)}
+            <div className="right">{group.reportCount}건</div>
           </div>
-        )}
 
-        <div className="info">
-          <div className="detail">
-            <Type />
-            재난 유형
+          <div className="info">
+            <div className="left">
+              <Clock />
+              발생 기간
+            </div>
+            <div className="right">
+              {formatSimpleDate(group.earliestReportTime, true)} ~{' '}
+              {formatSimpleDate(group.latestReportTime, true)}
+            </div>
           </div>
-          {group.disasterType.name}
-        </div>
-      </TitleWrapper>
+
+          {addressData && (
+            <div className="info">
+              <div className="left">
+                <Position />
+                발생 장소
+              </div>
+              <div className="right">{formatAddress(addressData, true)}</div>
+            </div>
+          )}
+        </InfoWrapper>
+      </DisasterWrapper>
 
       <Border />
 
       <ReportsWrapper>
         {group.reports.map((report) => (
           <ReportCard>
-            <div className="top">
-              <div className="number">제보 #{report.id}</div>
+            <div className="row">
+              <div className="report">제보 #{report.id}</div>
 
               {report.status === '검증됨' && (
                 <Tag variant="blue" iconTag={true}>
@@ -129,48 +142,57 @@ export const ModalReport = ({
               )}
             </div>
 
-            {report.trustScore && (
-              <div className="right">
-                <div className="detail">AI 신뢰도 점수</div>
-                <Tag variant={report.trustScore >= 75 ? 'black' : 'red'}>
-                  {report.trustScore}점
-                </Tag>
-              </div>
-            )}
-
             <div className="description">{report.disasterDescription}</div>
 
-            <div className="infos">
+            <InfoWrapper>
+              {report.trustScore && (
+                <div className="info">
+                  <div className="left">
+                    <Count />
+                    AI 신뢰도 점수
+                  </div>
+                  <Tag variant={report.trustScore >= 75 ? 'black' : 'red'}>
+                    {report.trustScore}점
+                  </Tag>
+                </div>
+              )}
+
               <div className="info">
-                <div className="detail">
+                <div className="left">
                   <Clock />
                   제보 시간
                 </div>
-                {formatDateTime(report.createdAt)}
+                <div className="right">{formatDateTime(report.createdAt)}</div>
               </div>
 
               <div className="info">
-                <div className="detail">
+                <div className="left">
                   <Position />
                   제보 장소
                 </div>
-                {report.address}
+                <div className="right">{report.address}</div>
               </div>
-            </div>
+
+              <div className="info">
+                <div className="left">
+                  <Clock />
+                  제보 위험도
+                </div>
+                <Tag variant={tag[report.riskLevel].variant}>
+                  {tag[report.riskLevel].level}
+                </Tag>
+              </div>
+            </InfoWrapper>
           </ReportCard>
         ))}
       </ReportsWrapper>
-
-      <Button variant="white" onClick={onClose}>
-        닫기
-      </Button>
     </ModalWrapper>
   );
 };
 
 const ModalWrapper = styled.div`
   padding: 20px;
-  width: 440px;
+  width: 560px;
   max-height: 75vh;
   display: flex;
   flex-direction: column;
@@ -180,59 +202,92 @@ const ModalWrapper = styled.div`
   border-radius: 12px;
   position: relative;
 
-  .title {
+  .row {
     display: flex;
     align-items: center;
     justify-content: space-between;
   }
+
+  .row2 {
+    display: flex;
+    align-items: center;
+    gap: 2px;
+  }
+
+  .row4 {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  }
+
+  .row10 {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .divider {
+    width: 1px;
+    height: 12px;
+    background: ${({ theme }) => theme.colors.gray500};
+  }
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
 
   .info {
     display: flex;
     align-items: center;
     gap: 10px;
-
-    color: ${({ theme }) => theme.colors.gray1000};
-    font-size: ${({ theme }) => theme.font.fontSize.detail12};
-    font-weight: ${({ theme }) => theme.font.fontWeight.medium};
   }
 
-  .detail {
+  .left {
     display: flex;
     align-items: center;
     gap: 2px;
 
     color: ${({ theme }) => theme.colors.gray700};
-    font-size: ${({ theme }) => theme.font.fontSize.detail12};
+    font-size: ${({ theme }) => theme.font.fontSize.body14};
     font-weight: ${({ theme }) => theme.font.fontWeight.medium};
   }
 
   .right {
-    display: flex;
-    align-items: center;
-    gap: 4px;
-
-    color: ${({ theme }) => theme.colors.gray800};
-    font-size: ${({ theme }) => theme.font.fontSize.detail12};
+    color: ${({ theme }) => theme.colors.gray1000};
+    font-size: ${({ theme }) => theme.font.fontSize.body14};
     font-weight: ${({ theme }) => theme.font.fontWeight.medium};
   }
 `;
 
-const TitleWrapper = styled.div`
+const DisasterWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 4px;
+  gap: 12px;
 
-  .top {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  .modal {
+    color: ${({ theme }) => theme.colors.gray1000};
+    font-size: ${({ theme }) => theme.font.fontSize.title22};
+    font-weight: ${({ theme }) => theme.font.fontWeight.medium};
   }
 
   .title {
-    color: ${({ theme }) => theme.colors.red600};
-    font-size: ${({ theme }) => theme.font.fontSize.title20};
+    margin-top: -4px;
+    color: ${({ theme }) => theme.colors.gray1000};
+    font-size: ${({ theme }) => theme.font.fontSize.head30};
     font-weight: ${({ theme }) => theme.font.fontWeight.bold};
   }
+
+  .tag {
+    color: ${({ theme }) => theme.colors.gray800};
+    font-size: ${({ theme }) => theme.font.fontSize.body14};
+    font-weight: ${({ theme }) => theme.font.fontWeight.medium};
+  }
+`;
+
+const ModalXImg = styled(ModalClose)`
+  cursor: pointer;
 `;
 
 const Border = styled.div`
@@ -282,11 +337,7 @@ const ReportCard = styled.div`
   border: 1.5px solid ${({ theme }) => theme.colors.gray400};
   border-radius: 8px;
 
-  .top {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
+  .report {
     color: ${({ theme }) => theme.colors.gray1000};
     font-size: ${({ theme }) => theme.font.fontSize.title20};
     font-weight: ${({ theme }) => theme.font.fontWeight.bold};
@@ -296,11 +347,5 @@ const ReportCard = styled.div`
     color: ${({ theme }) => theme.colors.gray700};
     font-size: ${({ theme }) => theme.font.fontSize.body16};
     font-weight: ${({ theme }) => theme.font.fontWeight.medium};
-  }
-
-  .infos {
-    display: flex;
-    flex-direction: column;
-    gap: 4px;
   }
 `;
